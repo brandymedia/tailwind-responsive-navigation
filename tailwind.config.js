11b63dc78c8012b5d9577476cabb1cce07c429a3
Andy Griffiths <andygriffiths@Andys-MacBook-Pro.local>
2021-05-25 14:21:34 +0100

    updated talwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ['*.html', 'public/dist/script.js'],
+  },
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
